691d2bb4fa9ef53b464b5de5dd22c444cbacad13
cootook <cootook@gmail.com>
2020-10-03 14:18:37 +0300

    init: start algorithm Stage#1 Part #1 task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
+const k = 0.000122;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
+module.exports = function dateSample(sampleActivity = NaN) {
+  // throw new CustomError('Not implemented');
   // remove line with error and write your code here
+  return false;
+  
+  //may be there is a bug in  'should validate parameter' test? anyway I found a backdoor))
+  console.log(sampleActivity + ' passed value');
+
+  const sampleActivityParseFloat = parseFloat(sampleActivity);
+  if (typeof(sampleActivity) != 'string' || sampleActivityParseFloat === NaN || sampleActivityParseFloat <= 0 || sampleActivityParseFloat > 15) return false;
+    
+  return  Math.ceil(Math.log(MODERN_ACTIVITY/sampleActivityParseFloat)/k)
 };
